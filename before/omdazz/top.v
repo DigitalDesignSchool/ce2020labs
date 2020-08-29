1c5e34e895e89fb7f8523b9779f9e5cc3780edd8
Yuri Panchul <yuri@panchul.com>
2020-08-28 23:19:11 -0700

    Added drafts to before directory

---
+++ before/omdazz/top.v
@@ -0,0 +1,31 @@
+module top
+(
+    input  [3:0] key_sw,
+    output [3:0] led,
+
+    output       buzzer
+);
+
+    wire a = ~ key_sw [0];
+    wire b = ~ key_sw [1];
+    
+    wire result = a ^ b;
+
+    assign led [0] = ~ result;
+    
+    assign led [1] = ~ (~ key_sw [0] ^ ~ key_sw [2]);
+
+    // Exercise 1: Change the code below.
+    // Write the same for led [2] - logic operation AND 
+	 // Write the same for led [3] - logic operation OR
+    
+    assign led [2] = 1'b0;
+    assign led [3] = 1'b1;
+
+    // Exercise 2: Change the code below.
+    // Turn on and off buzzer on the board using key.
+    // Listen to the click sound.
+
+    assign buzzer = key_sw [0];
+    
+endmodule
---
+++ before/zeowaa/top.v
@@ -0,0 +1,29 @@
+module top
+(
+    input  [ 3:0] key,
+    input  [ 7:0] sw,
+    output [11:0] led,
+    output        buzzer,
+);
+
+    wire a = ~ key [0];
+    wire b = ~ key [1];
+    
+    wire result = a ^ b;
+
+    assign led [0] = ~ result;
+    
+    assign led [1] = ~ (~ key [0] ^ ~ key [0]);
+
+    // Exercise 1: Change the code below.
+    // Write the same for AND and OR operations
+    
+    assign led [11:2] = 10'h3ff;
+
+    // Exercise 2: Change the code below.
+    // Turn on and off buzzer on the board using key.
+    // Listen to the click sound.
+
+    assign buzzer = 1'b1;
+
+endmodule
