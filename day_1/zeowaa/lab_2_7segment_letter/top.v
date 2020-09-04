b580eeb0b01e6ed7d3dee61a24d2b0862347bec1
Yuri Panchul <yuri@panchul.com>
2020-09-04 09:08:55 -0700

    copied day_1 files from omdazz to zeowaa

---
+++ day_1/zeowaa/lab_2_7segment_letter/top.v
@@ -0,0 +1,71 @@
+`include "config.vh"
+
+module top
+(
+    input        clk,
+    input        reset_n,
+    
+    input  [3:0] key_sw,
+    output [3:0] led,
+
+    output [7:0] abcdefgh,
+    output [3:0] digit,
+
+    output       buzzer,
+
+    output       hsync,
+    output       vsync,
+    output [2:0] rgb
+);
+
+    assign led    = 4'hf;
+    assign buzzer = 1'b0;
+    assign hsync  = 1'b1;
+    assign vsync  = 1'b1;
+    assign rgb    = 3'b0;
+
+    //   --a--
+    //  |     |
+    //  f     b
+    //  |     |
+    //   --g--
+    //  |     |
+    //  e     c
+    //  |     |
+    //   --d--  h
+    //
+    //  0 means light
+
+    parameter [7:0] C = 8'b01100011,
+                    E = 8'b01100001,
+                    h = 8'b11010001,
+                    I = 8'b11110011,
+                    P = 8'b00110001;
+
+    // assign abcdefgh = key_sw [0] ? C : E;
+    // assign digit    = key_sw [1] ? 4'b1110 : 4'b1101;
+
+    // Exercise 1: Display the first letters
+    // of your first name and last name instead.
+
+    // assign abcdefgh = ...
+    // assign digit    = ...
+
+    // Exercise 2: Display letters of a 4-character word
+    // using this code to display letter of ChIP as an example
+
+    reg [7:0] letter;
+    
+    always @*
+      case (key_sw)
+      4'b0111: letter = C;
+      4'b1011: letter = h;
+      4'b1101: letter = I;
+      4'b1110: letter = P;
+      default: letter = E;
+      endcase
+      
+    assign abcdefgh = letter;
+    assign digit    = key_sw == 4'b1111 ? 4'b0000 : key_sw;
+
+endmodule
---
+++ day_1/zeowaa/lab_4_shift_register/top.v
@@ -0,0 +1,62 @@
+`include "config.vh"
+
+module top
+(
+    input        clk,
+    input        reset_n,
+    
+    input  [3:0] key_sw,
+    output [3:0] led,
+
+    output [7:0] abcdefgh,
+    output [3:0] digit,
+
+    output       buzzer,
+
+    output       hsync,
+    output       vsync,
+    output [2:0] rgb
+);
+
+    assign abcdefgh  = 8'hff;
+    assign digit     = 4'hf;
+    assign buzzer    = 1'b0;
+    assign hsync     = 1'b1;
+    assign vsync     = 1'b1;
+    assign rgb       = 3'b0;
+    
+    //------------------------------------------------------------------------
+
+    reg [31:0] cnt;
+    
+    always @ (posedge clk or negedge reset_n)
+      if (~ reset_n)
+        cnt <= 32'b0;
+      else
+        cnt <= cnt + 32'b1;
+        
+    wire enable = (cnt [22:0] == 23'b0);
+
+    //------------------------------------------------------------------------
+
+    wire button_on = ~ key_sw [0];
+
+    reg [3:0] shift_reg;
+    
+    always @ (posedge clk or negedge reset_n)
+      if (~ reset_n)
+        shift_reg <= 4'b0;
+      else if (enable)
+        shift_reg <= { button_on, shift_reg [3:1] };
+
+    assign led = ~ shift_reg;
+
+    // Exercise 1: Make the light move in the opposite direction.
+
+    // Exercise 2: Make the light moving in a loop.
+    // Use another key to reset the moving lights back to no lights.
+
+    // Exercise 3: Display the state of the shift register
+    // on a seven-segment display, moving the light in a circle.
+
+endmodule
