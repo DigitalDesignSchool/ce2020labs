568e28a6a9e81bfaf9814ff84dce06044a7a57f5
Yuri Panchul <yuri@panchul.com>
2020-09-05 11:16:50 -0700

    removed extra stuff from basys3 and nexys4 wrappers still need to add signals and fill them

--- day_2/nexys4/lab_2_game/top.v
+++ day_2/nexys4/lab_2_game/top.v
@@ -35,33 +35,4 @@ module top
     inout  [12:1] jb
 );
 
-    wire unused = ^ { ja [4:1], ja [10:7], jb [4:1], jb [10:7] };
-    reg [31:0] n;
-    
-    always @ (posedge clk100mhz or negedge cpu_resetn)
-        if (! cpu_resetn)
-            n <= 32'b0;
-        else
-            n <= n + 1'b1;
-    
-    assign led = n [31:16] & sw [15:0];
-
-    assign led16_b = n [31] & btnc;
-    assign led16_g = n [30] & btnu;
-    assign led16_r = n [29] & btnl;
-    assign led17_b = n [28] & btnr;
-    assign led17_g = n [27] & btnd;
-    assign led17_r = n [26] & btnd;
-
-    assign ca = n [31];
-    assign cb = n [30];
-    assign cc = n [29];
-    assign cd = n [28];
-    assign ce = n [27];
-    assign cf = n [26];
-    assign cg = n [25];
-    assign dp = unused;
-
-    assign an = n [31:24];
-
 endmodule
