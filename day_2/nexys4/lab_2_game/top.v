7826e685f7b0cf10521a335d5dbf827a0f4c014a
i am <sergey.ivanets@gmail.com>
2020-09-13 21:35:19 +0300

    Pin settings changed

--- day_2/nexys4/lab_2_game/top.v
+++ day_2/nexys4/lab_2_game/top.v
@@ -6,27 +6,22 @@ module top
               strobe_to_update_xy_counter_width = 20
 )
 (
-    input        clk,
-    input        reset_n,
+    input CLK100MHZ,
+    input CPU_RESETN,
     
-    input  [3:0] key_sw,
-    output [3:0] led,
-
-    //output [7:0] abcdefgh,
-    //output [3:0] digit,
-
-    //output       buzzer,
-
-    output       hsync,
-    output       vsync,
-    output [2:0] rgb
+    input BTNL,
+    input BTNR,
+    
+    output  VGA_HS,
+    output  VGA_VS,
+    output	[3:0] VGA_R,	
+    output	[3:0] VGA_G,	
+    output	[3:0] VGA_B
 );
 
-//    assign led       = key_sw;
-//    assign abcdefgh  = { key_sw, key_sw };
-//    assign digit     = 4'b0;
-//    assign buzzer    = 1'b0;
 
+	 
+	wire [3:0] key_sw = {BTNL, 1'b1, BTNR, 1'b1};
     wire launch_key = key_sw != 4'b1111;  // Any key is pressed
     
     // Either of two leftmost keys is pressed
@@ -34,7 +29,11 @@ module top
 
     // Either of two rightmost keys is pressed
     wire right_key  = key_sw [1];
-
+	wire r,g,b;
+	assign VGA_R = {r,r,r,r};
+	assign VGA_G = {g,g,g,g};
+	assign VGA_B = {b,b,b,b};
+	
     game_top
     # (
         .clk_mhz (clk_mhz),
@@ -44,15 +43,15 @@ module top
     )
     i_game_top
     (
-        .clk              (   clk                   ),
-        .reset            ( ~ reset_n               ),
+        .clk              (   CLK100MHZ             ),
+        .reset            ( ~ CPU_RESETN            ),
 
         .launch_key       (   launch_key            ),
         .left_right_keys  ( { left_key, right_key } ),
 
-        .hsync            (   hsync                 ),
-        .vsync            (   vsync                 ),
-        .rgb              (   rgb                   )
+        .hsync            (   VGA_HS                ),
+        .vsync            (   VGA_VS                ),
+        .rgb              ( { r, g, b } )
     );
 
 endmodule
