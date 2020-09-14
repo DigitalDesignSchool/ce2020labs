63d28c2f6a30e34fcae808ed05d77767403a6fb2
Yuri Panchul <yuri@panchul.com>
2020-09-13 21:03:47 -0700

    cosmetic changes for pre-release

--- day_2/nexys4/lab_2_game/top.v
+++ day_2/nexys4/lab_2_game/top.v
@@ -1,39 +1,37 @@
 `include "config.vh"
-//Nexus A7 wrapper
+
+// Nexus A7 wrapper
+
 module top
 # (
     parameter clk_mhz = 100,
               strobe_to_update_xy_counter_width = 20
 )
 (
-    input CLK100MHZ,
-    input CPU_RESETN,
-    
-    input BTNL,
-    input BTNR,
-    
-    output  VGA_HS,
-    output  VGA_VS,
-    output	[3:0] VGA_R,	
-    output	[3:0] VGA_G,	
-    output	[3:0] VGA_B
+    input        CLK100MHZ,
+    input        CPU_RESETN,
+
+    input        BTNC,
+    input        BTNU,
+    input        BTNL,
+    input        BTNR,
+    input        BTND,
+
+    output       VGA_HS,
+    output       VGA_VS,
+    output [3:0] VGA_R,
+    output [3:0] VGA_G,
+    output [3:0] VGA_B
 );
 
+    wire r,g,b;
 
-	 
-	wire [3:0] key_sw = {BTNL, 1'b1, BTNR, 1'b1};
-    wire launch_key = key_sw != 4'b1111;  // Any key is pressed
-    
-    // Either of two leftmost keys is pressed
-    wire left_key   = key_sw [3] ;
+    assign VGA_R = { 4 { r } };
+    assign VGA_G = { 4 { g } };
+    assign VGA_B = { 4 { b } };
 
-    // Either of two rightmost keys is pressed
-    wire right_key  = key_sw [1];
-	wire r,g,b;
-	assign VGA_R = {r,r,r,r};
-	assign VGA_G = {g,g,g,g};
-	assign VGA_B = {b,b,b,b};
-	
+    wire any_key = BTNC | BTNU | BTNL | BTNR | BTND;
+
     game_top
     # (
         .clk_mhz (clk_mhz),
@@ -43,15 +41,15 @@ module top
     )
     i_game_top
     (
-        .clk              (   CLK100MHZ             ),
-        .reset            ( ~ CPU_RESETN            ),
+        .clk              (   CLK100MHZ     ),
+        .reset            ( ~ CPU_RESETN    ),
 
-        .launch_key       (   launch_key            ),
-        .left_right_keys  ( { left_key, right_key } ),
+        .launch_key       (   any_key       ),
+        .left_right_keys  ( { BTNL , BTNR } ),
 
-        .hsync            (   VGA_HS                ),
-        .vsync            (   VGA_VS                ),
-        .rgb              ( { r, g, b } )
+        .hsync            (   VGA_HS        ),
+        .vsync            (   VGA_VS        ),
+        .rgb              ( { r, g, b }     )
     );
 
 endmodule
--- scripts/nexys4/top.xdc
+++ scripts/nexys4/top.xdc
@@ -72,11 +72,11 @@ create_clock -add -name sys_clk_pin -period 10.00 -waveform {0 5} [get_ports {CL
 
 ##Buttons
 set_property -dict { PACKAGE_PIN C12   IOSTANDARD LVCMOS33 } [get_ports { CPU_RESETN }]; #IO_L3P_T0_DQS_AD1P_15 Sch=cpu_resetn
-#set_property -dict { PACKAGE_PIN N17   IOSTANDARD LVCMOS33 } [get_ports { BTNC }]; #IO_L9P_T1_DQS_14 Sch=btnc
-#set_property -dict { PACKAGE_PIN M18   IOSTANDARD LVCMOS33 } [get_ports { BTNU }]; #IO_L4N_T0_D05_14 Sch=btnu
+set_property -dict { PACKAGE_PIN N17   IOSTANDARD LVCMOS33 } [get_ports { BTNC }]; #IO_L9P_T1_DQS_14 Sch=btnc
+set_property -dict { PACKAGE_PIN M18   IOSTANDARD LVCMOS33 } [get_ports { BTNU }]; #IO_L4N_T0_D05_14 Sch=btnu
 set_property -dict { PACKAGE_PIN P17   IOSTANDARD LVCMOS33 } [get_ports { BTNL }]; #IO_L12P_T1_MRCC_14 Sch=btnl
 set_property -dict { PACKAGE_PIN M17   IOSTANDARD LVCMOS33 } [get_ports { BTNR }]; #IO_L10N_T1_D15_14 Sch=btnr
-#set_property -dict { PACKAGE_PIN P18   IOSTANDARD LVCMOS33 } [get_ports { BTND }]; #IO_L9N_T1_DQS_D13_14 Sch=btnd
+set_property -dict { PACKAGE_PIN P18   IOSTANDARD LVCMOS33 } [get_ports { BTND }]; #IO_L9N_T1_DQS_D13_14 Sch=btnd
 
 
 ##Pmod Headers
