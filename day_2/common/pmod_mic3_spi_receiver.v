c26ad93b9f661c3cb37d91d16521ed8a3a3171b4
Yuri Panchul <yuri@panchul.com>
2021-07-11 16:07:13 -0700

    Improved mic driver

--- day_2/common/pmod_mic3_spi_receiver.v
+++ day_2/common/pmod_mic3_spi_receiver.v
@@ -10,22 +10,22 @@ module pmod_mic3_spi_receiver
     output reg [15:0] value
 );
 
-    reg [15:0] cnt;
+    reg [ 6:0] cnt;
     reg [15:0] shift;
 
     always @ (posedge clock or posedge reset)
     begin
         if (reset)
-            cnt <= 16'b100;
+            cnt <= 7'b100;
         else
-            cnt <= cnt + 16'b1;
+            cnt <= cnt + 7'b1;
     end
 
-    assign sck = ~ cnt [3];
-    assign cs  =   cnt [8];
+    assign sck = ~ cnt [1];
+    assign cs  =   cnt [6];
 
-    wire sample_bit = ( cs == 1'b0 && cnt [3:0] == 4'b1111 );
-    wire value_done = ( cnt [8:0] == 9'b0 );
+    wire sample_bit = ( cs == 1'b0 && cnt [1:0] == 2'b11 );
+    wire value_done = ( cnt [6:0] == 7'b0 );
 
     always @ (posedge clock or posedge reset)
     begin
