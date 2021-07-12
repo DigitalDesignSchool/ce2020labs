e241e5806bffc1065a24805a614e3812120f09c4
Yuri Panchul <yuri@panchul.com>
2021-07-12 00:04:56 -0700

    Added support for rzrd board

--- scripts/create_run_directories.bash
+++ scripts/create_run_directories.bash
@@ -18,29 +18,49 @@ fi
 
 #-----------------------------------------------------------------------------
 
+"$find_to_run" ../day_1 ../day_2 -name omdazz  \
+| while read dir
+  do
+    copy_dir="${dir//omdazz/rzrd}"
+
+    printf "copying '%s' into '%s'\n" "$dir" "$copy_dir" 1>&2
+
+    if    ! rm -rf "$copy_dir" \
+       || ! cp -r "$dir" "$copy_dir"
+    then
+      printf "$script: cannot copy '%s' into '%s'\n" "$dir" "$copy_dir" 1>&2
+      exit 1
+    fi
+  done
+
+#-----------------------------------------------------------------------------
+
 "$find_to_run" ../day_1 ../day_2 -name top.v  \
-| sed "s/\/top\.v//" \
+| sed "s/\/top\.v//"  \
 | while read dir
   do
     case "$dir" in
     *omdazz*)          board="omdazz"    ;;
     *piswords*)        board="piswords"  ;;
+    *rzrd*)            board="rzrd"      ;;
     *zeowaa*)          board="zeowaa"    ;;
     *de10_lite*)       board="de10_lite" ;;
     *basys3*|*nexys4*) continue          ;;
 
     *)
-      printf "$script: unrecognized directory '%s'" "$dir" 1>&2
+      printf "$script: unrecognized directory '%s'\n" "$dir" 1>&2
       exit 1
     esac
 
-    echo $dir 1>&2
+    run_dir="$dir/run"
 
-    if    ! rm -rf "$dir/run" \
-       || ! mkdir  "$dir/run" \
-       || ! cp -r all_intel_fpga_boards/* $board/* "$dir/run"
+    printf "copying scripts into '%s'\n" "$run_dir" 1>&2
+
+    if    ! rm -rf "$run_dir" \
+       || ! mkdir  "$run_dir" \
+       || ! cp -r all_intel_fpga_boards/* $board/* "$run_dir"
     then
-      printf "$script: cannot copy into '%s'" "$dir/run" 1>&2
+      printf "$script: cannot copy into '%s'\n" "$run_dir" 1>&2
       exit 1
     fi
   done
